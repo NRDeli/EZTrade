50f4fc90237e16e0a7c66c680347e0b3c6a0c967
NRDeli <nirmitdeliwala@gmail.com>
2021-06-08 23:29:58 +0530

    Added listener for click

--- index.js
+++ index.js
@@ -13,7 +13,31 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    let companies = ["Aditya Birla Sun Life Mutual Fund",
+        "Axis Mutual Fund", "BNP Paribas Mutual Fund",
+        "BOI AXA Mutual Fund", "Baroda Mutual Fund",
+        "Canara Robeco Mutual Fund", "DSP Mutual Fund",
+        "Edelweiss Mutual Fund", "Franklin Templeton Mutual Fund",
+        "HDFC Mutual Fund", "HSBC Mutual Fund",
+        "ICICI Prudential Mutual Fund", "IDBI Mutual Fund",
+        "IDFC Mutual Fund", "IIFCL Mutual Fund (IDF)",
+        "IIFL Mutual Fund", "IL&FS Mutual Fund (IDF)",
+        "ITI Mutual Fund", "Indiabulls Mutual Fund",
+        "Invesco Mutual Fund", "JM Financial Mutual Fund",
+        "Kotak Mahindra Mutual Fund", "L&T Mutual Fund",
+        "LIC Mutual Fund", "Mahindra Manulife Mutual Fund",
+        "Mirae Asset Mutual Fund", "Motilal Oswal Mutual Fund",
+        "Navi Mutual Fund", "Nippon India Mutual Fund",
+        "PGIM India Mutual Fund", "PPFAS Mutual Fund",
+        "Principal Mutual Fund", "Quantum Mutual Fund",
+        "SBI Mutual Fund", "Shriram Mutual Fund",
+        "Sundaram Mutual Fund", "Tata Mutual Fund",
+        "Taurus Mutual Fund", "Trust Mutual Fund",
+        "UTI Mutual Fund", "Union Mutual Fund",
+        "YES Mutual Fund", "quant Mutual Fund",
+        "Pearson Specter Litt", "Dunder Mifflin"
+    ]
+    res.render('home', { companies: companies });
 });
 
 app.post('/coin/', (req, res) => {
@@ -21,9 +45,9 @@ app.post('/coin/', (req, res) => {
     res.render('info', { name: name });
 });
 
-app.get('/company/:id',(req,res)=>{
+app.get('/company/:id', (req, res) => {
     var company = req.params.id;
-    res.render('category',{ company: company })
+    res.render('category', { company: company })
 })
 
 app.listen(3000, () => {
--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,3 +1,27 @@
-let image = document.createElement('IMG');
-image.src = '/logos/tata.png';
-document.getElementById('imgparent').appendChild(image);
+// let image = document.createElement('IMG');
+// image.src = '/logos/tata.png';
+// document.getElementById('imgparent').appendChild(image);
+
+function expandandcollapse() {
+    document.getElementById('button_11').addEventListener('click', () => {
+        for (let i = 12; i <= 45; i++) {
+            let card = document.createElement('div');
+            div
+        }
+        // let head = document.createElement('H1');
+        // head.innerHTML = 'Y M C A';
+        // document.getElementById('letsappendithere').appendChild(head);
+    });
+    // document.getElementById('button_10').addEventListener('click', () => {
+    //     let
+    // });
+}
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+
