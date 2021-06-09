cb5a53e04263db1460815b1f7ac133be932f5e0c
NRDeli <nirmitdeliwala@gmail.com>
2021-06-09 20:58:16 +0530

    More info button works & error type 431 handled

--- index.js
+++ index.js
@@ -13,29 +13,20 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    let companies = ["Aditya Birla Sun Life Mutual Fund",
-        "Axis Mutual Fund", "BNP Paribas Mutual Fund",
-        "BOI AXA Mutual Fund", "Baroda Mutual Fund",
-        "Canara Robeco Mutual Fund", "DSP Mutual Fund",
-        "Edelweiss Mutual Fund", "Franklin Templeton Mutual Fund",
-        "HDFC Mutual Fund", "HSBC Mutual Fund",
-        "ICICI Prudential Mutual Fund", "IDBI Mutual Fund",
-        "IDFC Mutual Fund", "IIFCL Mutual Fund (IDF)",
-        "IIFL Mutual Fund", "IL&FS Mutual Fund (IDF)",
-        "ITI Mutual Fund", "Indiabulls Mutual Fund",
-        "Invesco Mutual Fund", "JM Financial Mutual Fund",
-        "Kotak Mahindra Mutual Fund", "L&T Mutual Fund",
-        "LIC Mutual Fund", "Mahindra Manulife Mutual Fund",
-        "Mirae Asset Mutual Fund", "Motilal Oswal Mutual Fund",
-        "Navi Mutual Fund", "Nippon India Mutual Fund",
-        "PGIM India Mutual Fund", "PPFAS Mutual Fund",
-        "Principal Mutual Fund", "Quantum Mutual Fund",
-        "SBI Mutual Fund", "Shriram Mutual Fund",
-        "Sundaram Mutual Fund", "Tata Mutual Fund",
-        "Taurus Mutual Fund", "Trust Mutual Fund",
-        "UTI Mutual Fund", "Union Mutual Fund",
-        "YES Mutual Fund", "quant Mutual Fund",
-        "Pearson Specter Litt", "Dunder Mifflin"
+    let companies = ["Aditya Birla Sun Life Mutual Fund", "Axis Mutual Fund", "BNP Paribas Mutual Fund",
+        "BOI AXA Mutual Fund", "Baroda Mutual Fund", "Canara Robeco Mutual Fund", "DSP Mutual Fund",
+        "Edelweiss Mutual Fund", "Franklin Templeton Mutual Fund", "HDFC Mutual Fund", "HSBC Mutual Fund",
+        "ICICI Prudential Mutual Fund", "IDBI Mutual Fund", "IDFC Mutual Fund", "IIFCL Mutual Fund (IDF)",
+        "IIFL Mutual Fund", "IL&FS Mutual Fund (IDF)", "ITI Mutual Fund", "Indiabulls Mutual Fund",
+        "Invesco Mutual Fund", "JM Financial Mutual Fund", "Kotak Mahindra Mutual Fund", "L&T Mutual Fund",
+        "LIC Mutual Fund", "Mahindra Manulife Mutual Fund", "Mirae Asset Mutual Fund", "Motilal Oswal Mutual Fund",
+        "Navi Mutual Fund", "Nippon India Mutual Fund", "PGIM India Mutual Fund", "PPFAS Mutual Fund",
+        "Principal Mutual Fund", "Quantum Mutual Fund", "SBI Mutual Fund", "Shriram Mutual Fund",
+        "Sundaram Mutual Fund", "Tata Mutual Fund", "Taurus Mutual Fund", "Trust Mutual Fund", "UTI Mutual Fund",
+        "Union Mutual Fund", "YES Mutual Fund", "quant Mutual Fund", "Pearson Specter Litt", "Dunder Mifflin"
+    ];
+    let logos = ["AdityaBirla.png", "axis.jpg", "baroda.png", "bnpparibas.png", "boi axa.jpg", ""
+
     ]
     res.render('home', { companies: companies });
 });
@@ -45,9 +36,9 @@ app.post('/coin/', (req, res) => {
     res.render('info', { name: name });
 });
 
-app.get('/company/:id', (req, res) => {
-    var company = req.params.id;
-    res.render('category', { company: company })
+app.get('/company/', (req, res) => {
+    var schemes = req.query.scheme;
+    res.render('category', { schemes: schemes })
 })
 
 app.listen(3000, () => {
--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,27 +1,80 @@
-// let image = document.createElement('IMG');
-// image.src = '/logos/tata.png';
-// document.getElementById('imgparent').appendChild(image);
-
 function expandandcollapse() {
-    document.getElementById('button_11').addEventListener('click', () => {
-        for (let i = 12; i <= 45; i++) {
-            let card = document.createElement('div');
-            div
+    document.getElementById('button_9').addEventListener('click', () => {
+
+        let image = document.createElement('img');
+        image.src = "/logos/axis.jpg";
+        image.classList.add('card-img-top');
+        document.getElementById('card_9').prepend(image);
+
+        let cardcontent = document.createElement('div');
+        cardcontent.classList.add('card-body');
+        document.getElementById('card_9').appendChild(cardcontent);
+
+        let cardcontentheading = document.createElement('h6');
+        cardcontentheading.classList.add('card-title');
+        cardcontentheading.innerHTML = "HDFC Mutual Fund";
+        document.getElementById('moreinfo').prepend(cardcontentheading);
+
+        document.getElementById('button_9').innerHTML = "Button 9";
+
+        document.getElementById('hidemeonclick').classList.toggle('visually-hidden');
+        document.getElementById('showmeonclick').classList.toggle('visually-hidden');
+        //Create a button that says 'I toggle now'
+        for (let i = 2; i < 9; i++) {
+            for (let j = 0; j < 5; j++) {
+                document.getElementById(`card_${5 * i + j}`).classList.toggle('visually-hidden');
+            }
         }
-        // let head = document.createElement('H1');
-        // head.innerHTML = 'Y M C A';
-        // document.getElementById('letsappendithere').appendChild(head);
     });
-    // document.getElementById('button_10').addEventListener('click', () => {
-    //     let
+    // document.getElementById('button_9').addEventListener('dblclick', () => {
+    //     //Delete everything, toggle the visually hidden classes. 
+    //     document.getElementById('card_9').removeChild()
     // });
 }
 
 
 
+///////////////////////////////////////////////////////////////////////
 
 
+function getapi(value) {
+    axios.get('https://latest-mutual-fund-nav.p.rapidapi.com/fetchLatestNAV', {
+        params: {
+            //Date: '04-Jun-2021',     //2007 onwards
+            // SchemeName: 'a',
+            //SchemeCode:'127629',
+            SchemeType: 'Open Ended Schemes',
+            MutualFundFamily: value,
+            //SchemeCategory:"Open Ended Schemes ( Equity Scheme - Large & Mid Cap Fund )"
+        },
+        headers: {
+            'x-rapidapi-key': 'c021a04ec2msh274866ae5d5f773p1d0d47jsn94fa2bd469a5',
+            'x-rapidapi-host': 'latest-mutual-fund-nav.p.rapidapi.com'
+        }
+    })
+        .then(function (response) {
+            var schemes = [];
+            for (let i = 0; i < response.data.length; i++) {
+                var schem = response.data[i]['Scheme Name'].toString();
+                // if (scheme.includes("&amp")) {
+                var scheme = schem.replace(/&/g, "and");
 
-/////////////////////////////////////////////////////////////////////////////////////////////////
+                if (scheme.includes("DIRECT") || scheme.includes("Direct")) {
 
+                }
+                else {
+                    schemes.push(scheme);
+                }
+
+
+            }
+            window.location.href = "http://localhost:3000/company?scheme=" + schemes;
+            //console.log(schemes);
+
+        })
+        .catch(function (e) {
+            console.log(e);
+
+        });
+}
 
